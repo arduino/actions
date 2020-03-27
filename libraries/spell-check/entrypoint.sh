a1d5b0aac907e592f93914206b99c3f9991e5b4c
per1234 <accounts@perglass.com>
2020-03-26 23:45:08 -0700

    Make libraries/spell-check's CODE_SPELL_ARGS variable an array (#25)
    Quoting the non-array variable caused the arguments to not be recognized by codespell. Making it an array fixes this issue, while still providing support for spaces, etc. in the input.

--- libraries/spell-check/entrypoint.sh
+++ libraries/spell-check/entrypoint.sh
@@ -2,10 +2,10 @@
 
 readonly IGNORE_WORDS_LIST="$1"
 
-CODE_SPELL_ARGS="--skip=.git"
+CODE_SPELL_ARGS=(--skip=.git)
 
 if test -f "$IGNORE_WORDS_LIST"; then
-  CODE_SPELL_ARGS="${CODE_SPELL_ARGS} --ignore-words=${IGNORE_WORDS_LIST}"
+  CODE_SPELL_ARGS=("${CODE_SPELL_ARGS[@]}" --ignore-words="${IGNORE_WORDS_LIST}")
 fi
 
-codespell "${CODE_SPELL_ARGS}" .
+codespell "${CODE_SPELL_ARGS[@]}" .
