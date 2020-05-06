303834867ea20699996aa00d9ef31494c7f42d31
per1234 <accounts@perglass.com>
2020-05-05 18:36:22 -0700

    libraries/spell-check: add skip-paths input
    This allows specifying paths to exclude from misspelled words check, which are added to codespell's --skip option.

--- libraries/spell-check/entrypoint.sh
+++ libraries/spell-check/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 readonly IGNORE_WORDS_LIST="$1"
+readonly SKIP_PATHS="$2"
 
-CODE_SPELL_ARGS=("--skip=.git")
+CODE_SPELL_ARGS=("--skip=${SKIP_PATHS},.git")
 
 if test -f "$IGNORE_WORDS_LIST"; then
   CODE_SPELL_ARGS=("${CODE_SPELL_ARGS[@]}" "--ignore-words=${IGNORE_WORDS_LIST}")
